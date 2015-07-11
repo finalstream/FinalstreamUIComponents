90ab1171ab077c80874d67e7b73955128720bbf4
finalstream <info@finalstream.net>
2015-07-11 21:25:44 +0900

    EventをEventArgs付きで処理できるコマンドを作成した。

---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FinalstreamUIComponents.Commands
+{
+    /// <summary>
+    /// EventをEventArgs付きで処理するためのコマンドを表します。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DelegateCommand<T> : ICommand
+    {
+        #region Fields
+
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+        public event EventHandler CanExecuteChanged;
+
+        #endregion
+
+        #region ICommand implementation
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+            return _canExecute((T) parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T) parameter);
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public DelegateCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+    }
+}
---
+++ Commands/InteractiveCommandAction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace FinalstreamUIComponents.Commands
+{
+    /// <summary>
+    /// EventをEventArg付きで処理するためのトリガーアクションを表します。
+    /// </summary>
+    public class InteractiveCommandAction : TriggerAction<DependencyObject>
+    {
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof (ICommand), typeof (InteractiveCommandAction),
+                new UIPropertyMetadata(null));
+
+        private string _commandName;
+
+        public string CommandName
+        {
+            get
+            {
+                ReadPreamble();
+                return _commandName;
+            }
+            set
+            {
+                if (CommandName != value)
+                {
+                    WritePreamble();
+                    _commandName = value;
+                    WritePostscript();
+                }
+            }
+        }
+
+        public ICommand Command
+        {
+            get { return (ICommand) GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        protected override void Invoke(object parameter)
+        {
+            if (AssociatedObject != null)
+            {
+                var command = ResolveCommand();
+                if ((command != null) && command.CanExecute(parameter))
+                {
+                    command.Execute(parameter);
+                }
+            }
+        }
+
+        private ICommand ResolveCommand()
+        {
+            ICommand command = null;
+            if (Command != null)
+            {
+                return Command;
+            }
+            if (AssociatedObject != null)
+            {
+                foreach (
+                    var info in AssociatedObject.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (typeof (ICommand).IsAssignableFrom(info.PropertyType) &&
+                        string.Equals(info.Name, CommandName, StringComparison.Ordinal))
+                    {
+                        command = (ICommand) info.GetValue(AssociatedObject, null);
+                    }
+                }
+            }
+            return command;
+        }
+    }
+}
