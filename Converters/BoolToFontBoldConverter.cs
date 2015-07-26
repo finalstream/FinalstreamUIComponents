3ec9d3b9ed6a9fdbe7bb96bc1ea1c651ef7594fd
finalstream <info@finalstream.net>
2015-07-26 21:38:44 +0900

    BoolToFontBoldConverter追加。

---
+++ Converters/BoolToFontBoldConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FinalstreamUIComponents.Converters
+{
+    public sealed class BoolToFontBoldConverter : BoolConverter<FontWeight>
+    {
+        public BoolToFontBoldConverter() :
+            base(FontWeights.Bold, SystemFonts.MessageFontWeight) { }
+    }
+}
