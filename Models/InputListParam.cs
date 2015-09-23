2a56a81ef5f22ad154b99a1b020e73e1a9927c9a
finalstream <info@finalstream.net>
2015-09-23 20:33:44 +0900

    こみっともれ。

---
+++ Models/InputListParam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalstreamUIComponents.Models
+{
+    public class InputListParam : InputParam
+    {
+        public InputListParam(string title, IEnumerable<string> values) : base(title, values)
+        {
+        }
+
+        public override InputType InputType
+        {
+            get { return InputType.List; }
+        }
+    }
+}
